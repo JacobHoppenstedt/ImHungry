e7cbb375c41dbd08b5202958d057abcb9648ceda
JacobHoppenstedt <jacobhoppenstedt@ufl.edu>
2023-12-01 17:14:03 -0500

    fix

--- recipe.py
+++ recipe.py
@@ -1,6 +1,6 @@
 class Recipe:
-    def __init__(self, name, time, rating, steps):
+    def __init__(self, name, time, rating, ingredients):
         self.name = name
         self.time = time
         self.rating = rating
-        self.steps = steps
+        self.ingredients = ingredients
