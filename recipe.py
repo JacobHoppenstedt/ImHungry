edaee4c4de26e0f6c9cadd5e359f510996a211d8
JacobHoppenstedt <jacobhoppenstedt@ufl.edu>
2023-12-02 12:47:24 -0500

    commit

--- recipe.py
+++ recipe.py
@@ -5,3 +5,21 @@ class Recipe:
         self.rating = rating
         self.ingredients = ingredients
 
+def read_recipe_csv(file_path):
+    recipes = []
+    with open(file_path, 'r') as file:
+        for line in file:
+
+            recipe_info = line.strip().split(', ')
+
+            name = recipe_info[0]
+            time = recipe_info[1]
+            rating = float(recipe_info[2])
+
+
+            ingredients = recipe_info[3].split('//')
+
+            recipe = Recipe(name, time, rating, ingredients)
+            recipes.append(recipe)
+
+    return recipes
