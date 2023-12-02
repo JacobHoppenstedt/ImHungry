6a48377df2c36d26199181281708f9ae28d7408d
JacobHoppenstedt <jacobhoppenstedt@ufl.edu>
2023-12-02 13:32:33 -0500

    test

--- recipe.py
+++ recipe.py
@@ -7,21 +7,3 @@ class Recipe:
 
     def getIngredients(self):
         return self.ingredients
-
-def read_recipe_csv(file_path):
-    recipes = []
-    with open(file_path, 'r') as file:
-        for line in file:
-
-            recipe_info = line.strip().split(', ')
-
-            name = recipe_info[0]
-            time = recipe_info[1]
-            rating = float(recipe_info[2])
-
-            ingredients = recipe_info[3].split('//')
-
-            recipe = Recipe(name, time, rating, ingredients)
-            recipes.append(recipe)
-
-    return recipes
