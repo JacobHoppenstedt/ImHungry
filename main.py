cb7c8c751a3cd5e53e2e61c473409ba5a9eb6345
Aryeh <aryehb113@gmail.com>
2023-12-03 15:14:42 -0500

    open multiple tabs

--- main.py
+++ main.py
@@ -55,6 +55,67 @@ def create_popup(item, cookbook):
 
     window.close()
 
+def open_search_type(type):
+    if type=='Meal Name':
+        layout = [
+            [sg.Text("Search for a meal...", size=(400, 1))],
+            [sg.Input(do_not_clear=True, size=(40, 2), enable_events=True, key='_INPUT_')],
+            [sg.Button('Sort by Rating', size=(20, 2), key='_SORT_BY_RATING_')],
+            [sg.Listbox(meal_names, size=(400, 400), enable_events=True, key='_LIST_')],
+        ]
+        tab_window = sg.Window(f'Search by {type}', layout, size=(800, 800), background_color='#F6F3E7')
+        while True:
+            event, values = tab_window.read()
+
+            if event in (sg.WIN_CLOSED, 'OK'):
+                break
+            if current_values['_INPUT_'] != '':
+                search_by_name(current_values['_INPUT_'], cookbook, tab_window)
+                sorted = False
+            elif current_values['_INPUT_'] == '' and sorted != True:
+                tab_window.Element('_LIST_').Update(meal_names)
+            if current_event == '_SORT_BY_RATING_':
+                sort_by_rating(cookbook, tab_window, layout)
+                sorted = True
+            if current_event == '_LIST_' and len(current_values['_LIST_']):
+                selected_item = current_values['_LIST_'][0]
+                create_popup(selected_item, cookbook)
+            if current_event == '_INGREDIENT_LIST_' and len(current_values['_INGREDIENT_LIST_']):
+                selected_item = current_values['_INGREDIENT_LIST_'][0]
+                create_popup(selected_item, cookbook)
+            if current_event == 'Back':
+                break
+        tab_window.close()
+    elif type=='Ingredients':
+        layout = [
+            [sg.Text("Search for recipes by ingredients...", size=(400, 1))],
+            [sg.Input(do_not_clear=True, size=(40, 2), enable_events=True, key='_INGREDIENT_INPUT_')],
+            [sg.Listbox([], size=(400, 400), enable_events=True, key='_INGREDIENT_LIST_')],
+        ]
+        tab_window = sg.Window(f'Search by {type}', layout, size=(800, 800), background_color='#F6F3E7')
+        while True:
+            event, values = tab_window.read()
+
+            if event in (sg.WIN_CLOSED, 'OK'):
+                break
+            if current_values['_INGREDIENT_INPUT_'] != '':
+                search_by_ingredients(current_values['_INGREDIENT_INPUT_'], cookbook, tab_window)
+            else:
+                tab_window.Element('_INGREDIENT_LIST_').Update([])
+
+            if current_event == '_LIST_' and len(current_values['_LIST_']):
+                selected_item = current_values['_LIST_'][0]
+                create_popup(selected_item, cookbook)
+            if current_event == '_INGREDIENT_LIST_' and len(current_values['_INGREDIENT_LIST_']):
+                selected_item = current_values['_INGREDIENT_LIST_'][0]
+                create_popup(selected_item, cookbook)
+            if current_event == 'Back':
+                break
+
+        tab_window.close()
+    
+    
+
 def search_by_name(search, cookbook, window):
     new_values = [x for x in meal_names if search.lower() in x.lower()]
     window.Element('_LIST_').Update(new_values)
@@ -81,25 +142,7 @@ cookbook.recipe_list = cookbook.recipe_list[5262:] + cookbook.recipe_list[:5262]
 # Populate recipe names into list
 meal_names = [recipe.name for recipe in cookbook.recipe_list]
 
-# Define the layout for each tab
-name_tab_layout = [
-    [sg.Text("Search for a meal...", size=(400, 1))],
-    [sg.Input(do_not_clear=True, size=(40, 2), enable_events=True, key='_INPUT_')],
-    [sg.Button('Sort by Rating', size=(20, 2), key='_SORT_BY_RATING_')],
-    [sg.Listbox(meal_names, size=(400, 400), enable_events=True, key='_LIST_')],
 
-]
-
-ingredient_tab_layout = [
-    [sg.Text("Search for recipes by ingredients...", size=(400, 1))],
-    [sg.Input(do_not_clear=True, size=(40, 2), enable_events=True, key='_INGREDIENT_INPUT_')],
-    [sg.Listbox([], size=(400, 400), enable_events=True, key='_INGREDIENT_LIST_')],
-]
-
-
-# Create separate windows for each tab
-name_tab_window = sg.Window('Search by Meal Name', name_tab_layout, size=(800, 800), background_color='#F6F3E7')
-ingredient_tab_window = sg.Window('Search by Ingredients', ingredient_tab_layout, size=(800, 800), background_color='#F6F3E7')
 logo_path = "images/logo.png"
 logo_image = Image.open(logo_path)
 
@@ -147,44 +190,8 @@ while True:
     
     # Open the respective search tab based on the button clicked
     if startup_event == 'Search by Recipe':
-        current_tab_layout = name_tab_layout
-        current_tab_window = name_tab_window
-    elif startup_event == 'Search by Ingredient':
-        current_tab_layout = ingredient_tab_layout
-        current_tab_window = ingredient_tab_window
-
-    while True:
-        current_event, current_values = current_tab_window.read(timeout=100)
-
-        if current_event == sg.WINDOW_CLOSED or current_event == 'Quit':
-            break
-        
-
-
-        if current_tab_layout is name_tab_layout:
-            if current_values['_INPUT_'] != '':
-                search_by_name(current_values['_INPUT_'], cookbook, current_tab_window)
-                sorted = False
-            elif current_values['_INPUT_'] == '' and sorted != True:
-                current_tab_window.Element('_LIST_').Update(meal_names)
-            if current_event == '_SORT_BY_RATING_':
-                sort_by_rating(cookbook, current_tab_window, current_tab_layout)
-                sorted = True
-        elif current_tab_layout is ingredient_tab_layout:
-            if current_values['_INGREDIENT_INPUT_'] != '':
-                search_by_ingredients(current_values['_INGREDIENT_INPUT_'], cookbook, current_tab_window)
-            else:
-                current_tab_window.Element('_INGREDIENT_LIST_').Update([])
-
-        if current_event == '_LIST_' and len(current_values['_LIST_']):
-            selected_item = current_values['_LIST_'][0]
-            create_popup(selected_item, cookbook)
-        if current_event == '_INGREDIENT_LIST_' and len(current_values['_INGREDIENT_LIST_']):
-            selected_item = current_values['_INGREDIENT_LIST_'][0]
-            create_popup(selected_item, cookbook)
-        if current_event == 'Back':
-            break
-
-    current_tab_window.close()
+        open_search_type('Meal Name')
+    if startup_event == 'Search by Ingredient':
+        open_search_type('Ingredients')
 
 startup_window.close()
